40aba47cec926486356cdd45573687afd697f816
zkdlu <zkdlu159951@gmail.com>
2020-12-26 02:46:46 +0900

    test

--- client/src/main/java/com/zkdlu/configclient/TestController.java
+++ client/src/main/java/com/zkdlu/configclient/TestController.java
@@ -1,16 +1,18 @@
 package com.zkdlu.configclient;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class TestController {
-    @Value("${hello.world}")
-    private String identity;
+    @Value("${hello.world:test}")
+    private String hello;
 
     @GetMapping("/test")
     public String test() {
-        return identity;
+        return hello;
     }
 }
