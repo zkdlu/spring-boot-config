7c27ab6f3154ff43b072ffa5b9773cd6c487754c
zkdlu <zkdlu159951@gmail.com>
2020-12-27 15:43:09 +0900

    config client

--- client/src/main/java/com/zkdlu/configclient/ConfigClientApplication.java
+++ client/src/main/java/com/zkdlu/configclient/ConfigClientApplication.java
@@ -2,8 +2,6 @@ package com.zkdlu.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ConfigClientApplication {
--- client/src/main/java/com/zkdlu/configclient/TestController.java
+++ client/src/main/java/com/zkdlu/configclient/TestController.java
@@ -1,18 +1,16 @@
 package com.zkdlu.configclient;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RefreshScope
 @RestController
 public class TestController {
-    @Value("${hello.world:test}")
-    private String hello;
+    @Value("${hello.world}")
+    private String test;
 
-    @GetMapping("/test")
-    public String test() {
-        return hello;
+    @GetMapping
+    public String hello() {
+        return test;
     }
 }
